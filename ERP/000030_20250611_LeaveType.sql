c637915572745d7da64f19ac32f968dd7b38c884
Tharaka Vinuth98 <132554036+Chipzy98@users.noreply.github.com>
2025-06-11 15:07:30 +0530

    Leave And Assignments

---
+++ ERP/000030_20250611_LeaveType.sql
@@ -0,0 +1,26 @@
+USE [ERP1]
+GO
+
+/****** Object:  Table [dbo].[hrm_LeaveType]    Script Date: 6/11/2025 2:50:52 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[hrm_LeaveType](
+	[LeaveTypeId] [uniqueidentifier] NOT NULL,
+	[LeaveCode] [nvarchar](50) NOT NULL,
+	[LeaveName] [nvarchar](100) NOT NULL,
+	[LeaveDescription] [nvarchar](max) NULL,
+	[IsUncountable] [bit] NOT NULL,
+	[NoOfDays] [int] NULL,
+	[SpanOverLeaveYear] [bit] NOT NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK_Leave_Type] PRIMARY KEY CLUSTERED 
+(
+	[LeaveTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
---
+++ ERP/000031_20250611_LeaveYear.sql
@@ -0,0 +1,22 @@
+USE [ERP1]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[hrm_LeaveYear](
+	[LeaveYearId] [uniqueidentifier] NOT NULL,
+	[LeaveYear] [nvarchar](20) NOT NULL,
+	[LeaveYearStartDate] [datetime] NOT NULL,
+	[LeaveYearEndDate] [datetime] NOT NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK_Leave_Year] PRIMARY KEY CLUSTERED 
+(
+	[LeaveYearId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
