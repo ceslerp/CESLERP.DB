4afbca235becf26e65ee017eb5df6e8136ab1b63
Chamil Gihan <81851245+chamilgihan@users.noreply.github.com>
2025-07-01 20:22:16 +0530

    Update 000036_20250621_AddCountryCoumnEmployeeVersion.sql

--- ERP/000036_20250621_AddCountryCoumnEmployeeVersion.sql
+++ ERP/000036_20250621_AddCountryCoumnEmployeeVersion.sql
@@ -11,30 +11,3 @@ GO
 ALTER TABLE cmn_EmployeeVersion
 ALTER COLUMN CountryId UNIQUEIDENTIFIER NOT NULL;
 GO
-
-ALTER TABLE cmn_EmployeeVersion
-ADD TitleInt INT;
-GO
-
-UPDATE cmn_EmployeeVersion
-SET TitleInt = CASE 
-    WHEN LOWER(Title) = 'mr' THEN 1
-    WHEN LOWER(Title) = 'miss' THEN 2
-    WHEN LOWER(Title) = 'mrs' THEN 3
-    ELSE NULL  -- You can choose how to handle unexpected values
-END;
-GO
-
-SELECT Title FROM cmn_EmployeeVersion
-WHERE TitleInt IS NULL
-GROUP BY Title;
-GO
-
--- Drop old Title column
-ALTER TABLE cmn_EmployeeVersion
-DROP COLUMN Title;
-GO
-
--- Rename the new column
-EXEC sp_rename 'dbo.cmn_EmployeeVersion.TitleInt', 'Title', 'COLUMN';
-GO
