2c18948340cd2a504ee72db332506fecf455657c
Kaveen Vishwajith <160998841+kaveevishwajith@users.noreply.github.com>
2025-07-24 10:38:16 +0530

    Update 000041_20250722_AddIsSiteEmpoyee_LeaveApplication.sql

--- ERP/000041_20250722_AddIsSiteEmpoyee_LeaveApplication.sql
+++ ERP/000041_20250722_AddIsSiteEmpoyee_LeaveApplication.sql
@@ -1,2 +1,10 @@
-ALTER TABLE [dbo].[hrm_LeaveApplication]
-ADD [IsSiteEmployee] [bit] NULL
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'hrm_LeaveApplication'
+      AND COLUMN_NAME = 'IsSiteEmployee'
+)
+BEGIN
+    ALTER TABLE [dbo].[hrm_LeaveApplication]
+    ADD [IsSiteEmployee] [bit] NULL;
+END
