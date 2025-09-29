630cf2dc0e52157271bf3138aa06dd8e02d9c51b
TharushiMadu <148357156+TharushiMadu@users.noreply.github.com>
2025-09-29 10:01:59 +0530

    Add files via upload

---
+++ ERP/000047_20250929_EmployeeTransition.sql
@@ -0,0 +1,59 @@
+USE [ERP]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[hrm_EmployeeTransition](
+    [EmployeeTransitionId] [uniqueidentifier] NOT NULL,
+    [EmployeeId] [uniqueidentifier] NOT NULL,
+    [TransitionTypeId] [uniqueidentifier] NOT NULL,
+    [COEUnitId] [uniqueidentifier] NULL,
+    [SOEUnitId] [uniqueidentifier] NULL,
+    [DesignationId] [uniqueidentifier] NULL,
+    [PostId] [uniqueidentifier] NULL,
+    [GradeId] [uniqueidentifier] NULL,
+    [Comment] [nvarchar](max) NULL,
+    [EffectiveDate] [datetime] NULL,
+    [DataStatus] [int] NOT NULL DEFAULT ((1)),
+    [CreatedDateTime] [datetime] NOT NULL,
+    [CreatedUserId] [uniqueidentifier] NOT NULL,
+    [UpdatedDateTime] [datetime] NULL,
+    [UpdatedUserId] [uniqueidentifier] NULL,
+    [ApprovedDateTime] [datetime] NULL,
+    [ApprovedUserId] [uniqueidentifier] NULL,
+    [TypeOfTransaction] [int] NULL,
+    [TypeOfRelease] [int] NULL,
+      NULL,
+    [LeaveStartDate] [datetime] NULL,
+    [LeaveEndDate] [datetime] NULL,
+      NULL,
+    [NoticePeriodStartDate] [datetime] NULL,
+    [NoticePeriodEndDate] [datetime] NULL,
+    [RetirementDate] [datetime] NULL,
+    [IsActive] [bit] NULL,
+      NULL,
+    [RejoinDate] [datetime] NULL,
+    [BasicSalary] [decimal](18, 2) NULL,
+    [AGMUnitId] [uniqueidentifier] NULL,
+    [DGMUnitId] [uniqueidentifier] NULL,
+    [PreviousCOEUnitId] [uniqueidentifier] NULL,
+    [PreviousSOEUnitId] [uniqueidentifier] NULL,
+    [PreviousGradeId] [uniqueidentifier] NULL,
+    [PreviousPostId] [uniqueidentifier] NULL,
+    [PreviousDesignationId] [uniqueidentifier] NULL,
+    CONSTRAINT [PK_hrm_EmployeeTransition] PRIMARY KEY CLUSTERED 
+    (
+        [EmployeeTransitionId] ASC
+    ) WITH (
+        PAD_INDEX = OFF, 
+        STATISTICS_NORECOMPUTE = OFF, 
+        IGNORE_DUP_KEY = OFF, 
+        ALLOW_ROW_LOCKS = ON, 
+        ALLOW_PAGE_LOCKS = ON
+    ) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
