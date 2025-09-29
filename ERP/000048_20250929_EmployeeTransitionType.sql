cb1052eb7e2bfd1ac610fb217b28415e6d2b16ae
TharushiMadu <148357156+TharushiMadu@users.noreply.github.com>
2025-09-29 16:31:40 +0530

    Update 000048_20250929_EmployeeTransitionType.sql

--- ERP/000048_20250929_EmployeeTransitionType.sql
+++ ERP/000048_20250929_EmployeeTransitionType.sql
@@ -7,30 +7,26 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[hrm_EmployeeTransitionType](
-    [EmployeeTransitionTypeId] [uniqueidentifier] NOT NULL DEFAULT (newsequentialid()),
-      NOT NULL,
-      NULL,
-    [IsActive] [bit] NOT NULL DEFAULT ((1)),
-    CONSTRAINT [PK_hrm_EmployeeTransitionType] PRIMARY KEY CLUSTERED 
-    (
-        [EmployeeTransitionTypeId] ASC
-    ) WITH (
-        PAD_INDEX = OFF, 
-        STATISTICS_NORECOMPUTE = OFF, 
-        IGNORE_DUP_KEY = OFF, 
-        ALLOW_ROW_LOCKS = ON, 
-        ALLOW_PAGE_LOCKS = ON
-    ) ON [PRIMARY],
-    CONSTRAINT [UQ_hrm_EmployeeTransitionType_Name] UNIQUE NONCLUSTERED 
-    (
-        [Name] ASC
-    ) WITH (
-        PAD_INDEX = OFF, 
-        STATISTICS_NORECOMPUTE = OFF, 
-        IGNORE_DUP_KEY = OFF, 
-        ALLOW_ROW_LOCKS = ON, 
-        ALLOW_PAGE_LOCKS = ON
-    ) ON [PRIMARY]
-) ON [PRIMARY]
+CREATE TABLE [dbo].[rq_RequestType](
+	[RequestTypeId] [uniqueidentifier] NOT NULL,
+	[GroupId] [uniqueidentifier] NULL,
+	[Description] [nvarchar](max) NULL,
+	[Active] [bit] NOT NULL DEFAULT ((1)),
+	[CodeNum] [int] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL DEFAULT (''),
+ CONSTRAINT [PK_Request_Type] PRIMARY KEY CLUSTERED 
+(
+	[RequestTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
 GO
+
+ALTER TABLE [dbo].[rq_RequestType]  WITH CHECK ADD  CONSTRAINT [FK_RequestType_RequestGroup] FOREIGN KEY([GroupId])
+REFERENCES [dbo].[rq_Request_Group] ([RequestGroupId])
+GO
+
+ALTER TABLE [dbo].[rq_RequestType] CHECK CONSTRAINT [FK_RequestType_RequestGroup]
+GO
+
+
