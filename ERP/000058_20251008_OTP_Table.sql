574dd27ba8566c575c73b91fd958b2f239a835b8
Kaveen Vishwajith <160998841+kaveevishwajith@users.noreply.github.com>
2025-10-13 17:44:14 +0530

    Update 000058_20251008_OTP_Table.sql

--- ERP/000058_20251008_OTP_Table.sql
+++ ERP/000058_20251008_OTP_Table.sql
@@ -1,21 +1,26 @@
 USE [ERP]
 GO
 
+/****** Object:  Table [dbo].[hrm_OTP]    Script Date: 10/13/2025 5:42:49 PM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[hrm_OTP](
-    [OTPId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
-    [EmployeeId] UNIQUEIDENTIFIER NOT NULL,
-    [OTP] NVARCHAR(6) NOT NULL,
-    [CreatedDateTime] DATETIME NOT NULL DEFAULT GETDATE(),
-    [IsVerified] BIT NOT NULL DEFAULT 0,
+	[OTPId] [uniqueidentifier] NOT NULL DEFAULT (newid()),
+	[EmployeeId] [uniqueidentifier] NOT NULL,
+	[OTP] [nvarchar](6) NOT NULL,
+	[CreatedDateTime] [datetime] NOT NULL DEFAULT (getdate()),
+	[IsVerified] [bit] NOT NULL DEFAULT ((0)),
+	[ExpireDateTime] [datetime] NULL,
  CONSTRAINT [PK_dbo.hrm_OTP] PRIMARY KEY CLUSTERED 
 (
-    [OTPId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-      IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[OTPId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
 GO
+
+
