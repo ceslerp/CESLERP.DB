2efc7800d80140bb2e5a108823bafa0b826ce84e
TharushiMadu <148357156+TharushiMadu@users.noreply.github.com>
2025-11-12 15:20:56 +0530

    Add files via upload

---
+++ ERP/000064_20251112_ContractRenewalApprovalEmailNotifications.sql
@@ -0,0 +1,88 @@
+USE [ERP];
+GO
+
+INSERT INTO [dbo].[cmn_EmailNotificationConfig] (
+    [Code],
+    [Name],
+    [TemplateFile],
+    [DataQuery],
+    [FromQuery],
+    [ToQuery],
+    [CcQuery],
+    [BccQuery],
+    [TupleParameters],
+    [Active],
+    [CreatedDate],
+    [UpdatedDate]
+)
+VALUES (
+    'ContractRenewalApproval',
+    N'Contract Renewal Approval',
+    N'ProjectNotifications.html',
+
+    -- Data Query
+    N'SELECT 
+        ''Contract Renewal'' AS Subject,
+        CONCAT(
+            ''Contract renewal for '', emp.NameWithInitial,
+            '' ('', emp.EPFNo, '')'',
+            '' - Designation: '', ISNULL(desig.DesignationName, ''N/A''),
+            '', Recruitment Period: '',
+            CONVERT(VARCHAR, cr.recruitmentStartDate, 103),
+            '' to '', CONVERT(VARCHAR, cr.recruitmentEndDate, 103),
+            ''. Agreement Date: '', CONVERT(VARCHAR, cr.AgrementDate, 103),
+            '' has been processed by '', appr.NameWithInitial,
+            '' ('', appr.EPFNo, '').''
+        ) AS BodyData,
+
+        cr.ContractRenewId,
+        cr.EmployeeId,
+        emp.NameWithInitial AS EmployeeName,
+        emp.EPFNo AS EmployeeEPFNo,
+        ISNULL(desig.DesignationName, ''N/A'') AS Designation,
+        cr.recruitmentStartDate,
+        cr.recruitmentEndDate,
+        cr.AgrementDate,
+        appr.NameWithInitial AS ProcessedByName,
+        appr.EPFNo AS ProcessedByEPFNo
+
+    FROM hrm_ContractRenew cr
+    LEFT JOIN cmn_EmployeeVersion emp 
+        ON emp.EmployeeId = cr.EmployeeId 
+        AND emp.DataStatus = 5
+    LEFT JOIN cmn_EmployeeVersion appr 
+        ON appr.EmployeeId = @userEmployeeId 
+        AND appr.DataStatus = 5
+    LEFT JOIN hrm_Designation desig 
+        ON desig.DesignationId = cr.Designation
+    WHERE cr.ContractRenewId = @ContractRenewId;',
+
+    -- From Query
+    N'SELECT ''notifications.ceslerp@gmail.com'' AS [Email];',
+
+    -- To Query
+    N'SELECT DISTINCT 
+        rv.NameWithInitial, 
+        rv.Email
+    FROM EmployeeRoleView rv
+    WHERE rv.RoleCode = ''COE''
+      AND rv.Email IS NOT NULL
+      AND rv.EmployeeId IN (
+          SELECT ws.HeadOfWorkSpace
+          FROM cmn_EmployeeVersion ev
+          INNER JOIN cmn_WorkSpace ws 
+              ON ws.WorkSpaceId = ev.WorkSpaceId
+          WHERE ev.EmployeeId = @userEmployeeId
+            AND ev.DataStatus = 5
+            AND ws.HeadOfWorkSpace IS NOT NULL
+            AND ws.HeadOfWorkSpace != ev.EmployeeId
+      );',
+
+    NULL,  -- CcQuery
+    NULL,  -- BccQuery
+    N'<ContractRenewId,guid><userEmployeeId,guid>',  -- Tuple Parameters
+    1,  -- Active
+    GETDATE(),  -- CreatedDate
+    GETDATE()   -- UpdatedDate
+);
+GO
---
+++ ERP/000065_20251112_ContractRenewalApprovedEmailNotifications.sql
@@ -0,0 +1,107 @@
+USE [ERP];
+GO
+
+INSERT INTO [dbo].[cmn_EmailNotificationConfig] (
+    [Code],
+    [Name],
+    [TemplateFile],
+    [DataQuery],
+    [FromQuery],
+    [ToQuery],
+    [CcQuery],
+    [BccQuery],
+    [TupleParameters],
+    [Active],
+    [CreatedDate],
+    [UpdatedDate]
+)
+VALUES (
+    'ContractRenewalApproved',
+    N'Contract Renewal Approved',
+    N'ProjectNotifications.html',
+
+    -- Data Query
+    N'SELECT 
+        ''Contract Renewal Approved'' AS Subject,
+        CONCAT(
+            ''The contract renewal for '', emp.NameWithInitial,
+            '' ('', emp.EPFNo, '') has been approved.'',
+            CHAR(13) + CHAR(10) + CHAR(13) + CHAR(10),
+            '' Employee: '', emp.NameWithInitial, '' ('', emp.EPFNo, '')'',
+            CHAR(13) + CHAR(10),
+            '' Designation: '', ISNULL(desig.DesignationName, ''N/A''),
+            CHAR(13) + CHAR(10),
+            '' Recruitment Period: '', 
+                CONVERT(VARCHAR, cr.recruitmentStartDate, 106), 
+                '' to '', CONVERT(VARCHAR, cr.recruitmentEndDate, 106),
+            CHAR(13) + CHAR(10),
+            '' Agreement Date: '', CONVERT(VARCHAR, cr.AgrementDate, 106),
+            CHAR(13) + CHAR(10),
+            '' Approved By: '', appr.NameWithInitial, '' ('', appr.EPFNo, '')'',
+            CHAR(13) + CHAR(10),
+            '' Approval Date: '', CONVERT(VARCHAR, GETDATE(), 106),
+            CHAR(13) + CHAR(10) + CHAR(13) + CHAR(10),
+            ''This contract renewal has been successfully processed in the system.''
+        ) AS BodyData,
+
+        cr.ContractRenewId,
+        cr.EmployeeId,
+        emp.NameWithInitial AS EmployeeName,
+        emp.EPFNo AS EmployeeEPFNo,
+        emp.Email AS EmployeeEmail,
+        ISNULL(desig.DesignationName, ''N/A'') AS Designation,
+        cr.recruitmentStartDate,
+        cr.recruitmentEndDate,
+        cr.AgrementDate,
+        appr.NameWithInitial AS ApprovedByName,
+        appr.EPFNo AS ApprovedByEPFNo,
+        appr.Email AS ApprovedByEmail,
+        GETDATE() AS ApprovalDate
+
+    FROM hrm_ContractRenew cr
+    INNER JOIN cmn_EmployeeVersion emp 
+        ON emp.EmployeeId = cr.EmployeeId 
+        AND emp.DataStatus = 5
+    INNER JOIN cmn_EmployeeVersion appr 
+        ON appr.EmployeeId = @userEmployeeId 
+        AND appr.DataStatus = 5
+    LEFT JOIN hrm_Designation desig 
+        ON desig.DesignationId = cr.Designation
+    WHERE cr.ContractRenewId = @ContractRenewId;',
+
+    -- From Query
+    N'SELECT ''notifications.ceslerp@gmail.com'' AS [Email];',
+
+    -- To Query (Creator and Affected Employee)
+    N'SELECT 
+        ''Creator'' AS Type,
+        ev.NameWithInitial,
+        ev.PrivateEmail AS Email,
+        ev.EPFNo
+    FROM cmn_EmployeeVersion ev
+    WHERE ev.EmployeeId = @userEmployeeId
+      AND ev.DataStatus = 5
+
+    UNION ALL
+
+    SELECT 
+        ''Affected Employee'' AS Type,
+        ev.NameWithInitial,
+        ev.PrivateEmail AS Email,
+        ev.EPFNo
+    FROM cmn_EmployeeVersion ev
+    WHERE ev.EmployeeId = (
+        SELECT cr.EmployeeId
+        FROM hrm_ContractRenew cr
+        WHERE cr.ContractRenewId = @ContractRenewId
+    )
+      AND ev.DataStatus = 5;',
+
+    NULL,  -- CcQuery
+    NULL,  -- BccQuery
+    N'<ContractRenewId,guid><userEmployeeId,guid>',  -- Tuple Parameters
+    1,  -- Active
+    GETDATE(),  -- CreatedDate
+    GETDATE()   -- UpdatedDate
+);
+GO
