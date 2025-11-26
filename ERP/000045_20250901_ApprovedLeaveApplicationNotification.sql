b938a5c7432d0e2dc66ffe54a6ece59ae5a6f89a
Kaveen Vishwajith <160998841+kaveevishwajith@users.noreply.github.com>
2025-11-26 13:58:56 +0530

    Update 000045_20250901_ApprovedLeaveApplicationNotification.sql

--- ERP/000045_20250901_ApprovedLeaveApplicationNotification.sql
+++ ERP/000045_20250901_ApprovedLeaveApplicationNotification.sql
@@ -15,9 +15,11 @@ BEGIN
             ,[CreatedDate]
             ,[UpdatedDate])
         VALUES
-            ('LeaveApproved', -- Code
+        (
+            'LeaveApproved', -- Code
             'Leave Application Approved', -- Name
             'ProjectNotifications.html', -- TemplateFile
+      
             'SELECT ''Leave is Approved'' AS Subject,
                    CONCAT(
                        ''Leave application from '', emp.NameWithInitial,
@@ -37,45 +39,61 @@ BEGIN
                    la.LeaveEndDate,
                    la.NoOfDays,
                    appr.NameWithInitial AS ApproverName,
-                   appr.EPFNo AS ApproverEPFNo
+                   appr.EPFNo AS ApproverEPFNo,
+
+                   -- NEW FIELD (only for covering employee) – simplified
+                   CASE 
+                       WHEN la.CoveringEmployeeId IS NOT NULL 
+                       THEN ''You are the covering employee for '' + emp.NameWithInitial + ''.''
+                       ELSE NULL
+                   END AS CoveringMessage
             FROM hrm_LeaveApplication la
-            LEFT JOIN cmn_EmployeeVersion emp ON emp.EmployeeId = la.EmployeeId 
-                AND emp.DataStatus = 5
-            LEFT JOIN cmn_EmployeeVersion appr ON appr.EmployeeId = @userEmployeeId 
-                AND appr.DataStatus = 5
-            LEFT JOIN hrm_LeaveType lt ON lt.LeaveTypeId = la.LeaveTypeId
-            WHERE la.LeaveApplicationId = @leaveApplicationId', -- DataQuery
-            'SELECT ''notifications.ceslerp@gmail.com'' AS [Email]', -- FromQuery
-            'SELECT ev.NameWithInitial, ev.PrivateEmail AS Email
+            LEFT JOIN cmn_EmployeeVersion emp 
+                 ON emp.EmployeeId = la.EmployeeId AND emp.DataStatus = 5
+            LEFT JOIN cmn_EmployeeVersion appr
+                 ON appr.EmployeeId = @userEmployeeId AND appr.DataStatus = 5
+            LEFT JOIN hrm_LeaveType lt
+                 ON lt.LeaveTypeId = la.LeaveTypeId
+            WHERE la.LeaveApplicationId = @leaveApplicationId',
+
+            'SELECT ''notifications.ceslerp@gmail.com'' AS [Email]',
+    
+            'SELECT ev.NameWithInitial,
+                    ev.PrivateEmail AS Email
              FROM cmn_EmployeeVersion ev
              WHERE ev.EmployeeId = (
-                 SELECT la.EmployeeId 
-                 FROM hrm_LeaveApplication la 
+                 SELECT la.EmployeeId
+                 FROM hrm_LeaveApplication la
                  WHERE la.LeaveApplicationId = @leaveApplicationId
              )
-             AND ev.DataStatus = 5
-             UNION ALL
-             SELECT ev.NameWithInitial, ev.PrivateEmail AS Email
-             FROM cmn_EmployeeVersion ev
-             WHERE ev.EmployeeId = (
-                 SELECT la.CoveringEmployeeId 
-                 FROM hrm_LeaveApplication la 
-                 WHERE la.LeaveApplicationId = @leaveApplicationId
-                   AND la.CoveringEmployeeId IS NOT NULL
-             )
-             AND ev.DataStatus = 5', -- ToQuery
-            NULL, -- CcQuery
-            NULL, -- BccQuery
-            '<leaveApplicationId,guid><userEmployeeId,guid>', -- Parameters
-            1, -- Active
-            GETDATE(), -- CreatedDate
-            GETDATE()); -- UpdatedDate
+             AND ev.DataStatus = 5',
+
+            'SELECT 
+                ev.NameWithInitial,
+                ev.PrivateEmail AS Email,
+                ''You are the covering employee for '' + emp.NameWithInitial + ''.'' AS CoveringMessage
+             FROM hrm_LeaveApplication la
+             INNER JOIN cmn_EmployeeVersion ev 
+                 ON ev.EmployeeId = la.CoveringEmployeeId AND ev.DataStatus = 5
+             INNER JOIN cmn_EmployeeVersion emp 
+                 ON emp.EmployeeId = la.EmployeeId AND emp.DataStatus = 5
+             WHERE la.LeaveApplicationId = @leaveApplicationId
+               AND la.CoveringEmployeeId IS NOT NULL',
+
+            NULL,
+
+            '<leaveApplicationId,guid><userEmployeeId,guid>',
+
+            1,
+            GETDATE(),
+            GETDATE()
+        );
 
         PRINT 'Insert successful into cmn_EmailNotificationConfig.';
     END
     ELSE
     BEGIN
-        PRINT 'Error: Record with Code ''LeaveApproved'' already exists in cmn_EmailNotificationConfig.';
+        PRINT ''Error: LeaveApproved notification already exists.'';';
     END
 END
 GO
