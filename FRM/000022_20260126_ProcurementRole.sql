ee763d3e5d3afa3a056738097f84334fde1c5442
Tharaka Vinuth98 <132554036+Chipzy98@users.noreply.github.com>
2026-01-26 12:10:01 +0530

    Refactor SQL script for Procurement roles

--- FRM/000022_20260126_ProcurementRole.sql
+++ FRM/000022_20260126_ProcurementRole.sql
@@ -50,28 +50,3 @@ BEGIN
          NULL, NULL)
 END
 GO
-
--- Add PORequisition
---IF NOT EXISTS (
---    SELECT 1 
---    FROM [dbo].[TSys_Role] 
---    WHERE RoleId = '7c04deea-86e7-4d70-980a-e5174f960121'
---)
---BEGIN
---    INSERT INTO [dbo].[TSys_Role]
---        ([RoleId], [RoleCode], [RoleName], [Description], [IsActive],
---         [OrganizationId], [OrganizationCode], [BusinessUnitId], [BusinessUnitCode],
---         [CreatedDateTime], [CreatedUserId], [UpdatedDateTime], [UpdatedUserId])
---    VALUES
---        ('7c04deea-86e7-4d70-980a-e5174f960121',
---         'Add PORequisition',
---         'Add PORequisition',
---         'Add PORequisition',
---         1,
---         'bae027a7-2fa5-41ba-8753-1450fd21b181', 'O0001',
---         'f8ad62ee-8fc1-41c8-bf8d-2f1dfebd1d0a', 'B0001',
---         '2026-01-23 08:45:57.853',
---         '8c98df6d-95dd-456c-a649-db7393465536',
---         NULL, NULL)
---END
---GO
