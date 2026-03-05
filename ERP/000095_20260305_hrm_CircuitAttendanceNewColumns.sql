61e992c84516b0f4e1801b9236362f0620df7875
Kaveen Vishwajith <160998841+KaveenVishwajith@users.noreply.github.com>
2026-03-05 12:20:35 +0530

    Create 000095_20260305_hrm_CircuitAttendanceNewColumns.sql

---
+++ ERP/000095_20260305_hrm_CircuitAttendanceNewColumns.sql
@@ -0,0 +1,32 @@
+-- AttendanceTypeId
+IF COL_LENGTH('dbo.hrm_CircuitAttendance', 'AttendanceTypeId') IS NULL
+BEGIN
+    ALTER TABLE dbo.hrm_CircuitAttendance
+    ADD AttendanceTypeId UNIQUEIDENTIFIER NULL;
+END
+
+
+-- Approval / Reject / Discard columns
+IF COL_LENGTH('dbo.hrm_CircuitAttendance', 'ApprovedDateTime') IS NULL
+BEGIN
+    ALTER TABLE dbo.hrm_CircuitAttendance
+    ADD
+        ApprovedDateTime DATETIME NULL,
+        ApprovedUserId UNIQUEIDENTIFIER NULL,
+
+        RejectedDateTime DATETIME NULL,
+        RejectedUserId UNIQUEIDENTIFIER NULL,
+
+        DiscardedDateTime DATETIME NULL,
+        DiscardedUserId UNIQUEIDENTIFIER NULL;
+END
+
+
+-- Update tracking columns
+IF COL_LENGTH('dbo.hrm_CircuitAttendance', 'UpdatedUserId') IS NULL
+BEGIN
+    ALTER TABLE dbo.hrm_CircuitAttendance
+    ADD
+        UpdatedUserId UNIQUEIDENTIFIER NULL,
+        UpdatedDateTime DATETIME NULL;
+END
