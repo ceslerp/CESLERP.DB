25a49571e93943f9b5abebe91ee1b6bb01b267fd
Kaveen Vishwajith <160998841+KaveenVishwajith@users.noreply.github.com>
2026-03-08 12:12:23 +0530

    Update 000095_20260305_hrm_CircuitAttendanceNewColumns.sql

--- ERP/000095_20260305_hrm_CircuitAttendanceNewColumns.sql
+++ ERP/000095_20260305_hrm_CircuitAttendanceNewColumns.sql
@@ -30,3 +30,16 @@ BEGIN
         UpdatedUserId UNIQUEIDENTIFIER NULL,
         UpdatedDateTime DATETIME NULL;
 END
+
+    --Reject Comment and Discard Comment
+IF COL_LENGTH('dbo.hrm_CircuitAttendance', 'RejectComment') IS NULL
+BEGIN
+    ALTER TABLE dbo.hrm_CircuitAttendance
+    ADD RejectComment VARCHAR(MAX) NULL;
+END
+
+IF COL_LENGTH('dbo.hrm_CircuitAttendance', 'DiscardComment') IS NULL
+BEGIN
+    ALTER TABLE dbo.hrm_CircuitAttendance
+    ADD DiscardComment VARCHAR(MAX) NULL;
+END
