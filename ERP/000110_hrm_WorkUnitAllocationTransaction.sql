bb7a9fe0a488cf46726a190c2600a55ed0bb2227
chamodminidu <miniduchamod0710@gmail.com>
2026-03-18 10:48:58 +0530

    Add IsActive column to hrm_HRMWorkUnitAllocationTransaction

--- ERP/000110_hrm_WorkUnitAllocationTransaction.sql
+++ ERP/000110_hrm_WorkUnitAllocationTransaction.sql
@@ -1,4 +1,12 @@
-ALTER TABLE [dbo].[hrm_HRMWorkUnitAllocationTransaction]
-ADD [IsActive] [bit] NOT NULL
-    CONSTRAINT [DF_hrm_HRMWorkUnitAllocationTransaction_IsActive] DEFAULT ((1))
+IF NOT EXISTS (
+    SELECT 1 
+    FROM sys.columns 
+    WHERE Name = 'IsActive'
+      AND Object_ID = Object_ID('dbo.hrm_HRMWorkUnitAllocationTransaction')
+)
+BEGIN
+    ALTER TABLE [dbo].[hrm_HRMWorkUnitAllocationTransaction]
+    ADD [IsActive] [bit] NOT NULL 
+        CONSTRAINT [DF_hrm_HRMWorkUnitAllocationTransaction_IsActive] DEFAULT ((1));
+END
 GO
