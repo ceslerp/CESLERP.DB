f4926ddd38a28305128646eafa5c6c33a05bf21b
TharushiMadu <148357156+TharushiMadu@users.noreply.github.com>
2026-05-07 23:45:47 +0530

    Rename 000120_20260507_SPGetPMMergeWorkspace.sql to 000121_20260507_SPGetPMMergeWorkspace.sql

---
+++ ERP/000121_20260507_SPGetPMMergeWorkspace.sql
@@ -0,0 +1,36 @@
+﻿USE [ERP]
+GO
+/****** Object:  StoredProcedure [dbo].[pm_SPGetMergeWorkspace]    Script Date: 5/7/2026 11:19:29 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- EXEC [cmn_SPGetMergeWorkspace] '061e523f-e442-47ba-a523-737c2a38986d'
+
+ALTER PROCEDURE [dbo].[pm_SPGetMergeWorkspace] 
+    @employeeId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SELECT MER.WorkSpaceId
+	INTO #TempWorkspace
+	FROM pm_MergeWorkSpace MER
+	WHERE MER.EmployeeId = @employeeId
+
+	DECLARE @IntCnt INT
+	SELECT @IntCnt = COUNT(WorkSpaceId) FROM #TempWorkspace
+	
+	IF(@IntCnt = 0)
+	BEGIN
+		DECLARE @EmpWorkSpaceId UNIQUEIDENTIFIER;
+				
+			SELECT @EmpWorkSpaceId = WorkSpaceId
+			FROM cmn_EmployeeVersion WHERE EmployeeId = @employeeId
+
+			INSERT INTO #TempWorkspace VALUES (@EmpWorkSpaceId)
+		
+	END
+
+	SELECT *
+	FROM #TempWorkspace
+END
