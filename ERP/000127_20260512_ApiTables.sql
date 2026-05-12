b2e6999fda4af63829a9c6d50c8f86c3c3208f72
Harshan Nishantha <hn44447@gmail.com>
2026-05-12 13:15:37 +0530

    api tables

---
+++ ERP/000127_20260512_ApiTables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [dbo].[api_ApiUser] (
+    [ApiUserId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [DisplayName] NVARCHAR(255) NOT NULL,
+    [ClientID] NVARCHAR(255) NOT NULL,
+    [ClientSecret] NVARCHAR(512) NOT NULL,
+    [IsActive] BIT NOT NULL
+);
+
+CREATE TABLE [dbo].[api_ApiScopes] (
+    [ApiScopeId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [ScopeName] NVARCHAR(255) NOT NULL,
+    [ScopeDecription] NVARCHAR(1000) NULL
+);
+
+CREATE TABLE [dbo].[api_ApiUserScopes] (
+    [ApiUserId] UNIQUEIDENTIFIER NOT NULL,
+    [ApiScopeId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_api_ApiUserScopes] PRIMARY KEY CLUSTERED ([ApiUserId], [ApiScopeId]),
+    CONSTRAINT [FK_api_ApiUserScopes_ApiUser] FOREIGN KEY ([ApiUserId]) REFERENCES [dbo].[api_ApiUser]([ApiUserId]),
+    CONSTRAINT [FK_api_ApiUserScopes_ApiScopes] FOREIGN KEY ([ApiScopeId]) REFERENCES [dbo].[api_ApiScopes]([ApiScopeId])
+);
