4c339ec6a814764becdfa25104136c40ac9170d7
Harshan Nishantha <hn44447@gmail.com>
2026-05-15 22:51:57 +0530

    api tables typo fixed

--- ERP/000127_20260512_ApiTables.sql
+++ ERP/000127_20260512_ApiTables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[api_ApiUser] (
     [ApiUserId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
     [DisplayName] NVARCHAR(255) NOT NULL,
-    [ClientID] NVARCHAR(255) NOT NULL,
+    [ClientId] NVARCHAR(255) NOT NULL,
     [ClientSecret] NVARCHAR(512) NOT NULL,
     [IsActive] BIT NOT NULL
 );
@@ -9,13 +9,20 @@ CREATE TABLE [dbo].[api_ApiUser] (
 CREATE TABLE [dbo].[api_ApiScopes] (
     [ApiScopeId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
     [ScopeName] NVARCHAR(255) NOT NULL,
-    [ScopeDecription] NVARCHAR(1000) NULL
+    [ScopeDescription] NVARCHAR(1000) NULL
 );
 
 CREATE TABLE [dbo].[api_ApiUserScopes] (
     [ApiUserId] UNIQUEIDENTIFIER NOT NULL,
     [ApiScopeId] UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [PK_api_ApiUserScopes] PRIMARY KEY CLUSTERED ([ApiUserId], [ApiScopeId]),
-    CONSTRAINT [FK_api_ApiUserScopes_ApiUser] FOREIGN KEY ([ApiUserId]) REFERENCES [dbo].[api_ApiUser]([ApiUserId]),
-    CONSTRAINT [FK_api_ApiUserScopes_ApiScopes] FOREIGN KEY ([ApiScopeId]) REFERENCES [dbo].[api_ApiScopes]([ApiScopeId])
+    CONSTRAINT [PK_api_ApiUserScopes] 
+        PRIMARY KEY CLUSTERED ([ApiUserId], [ApiScopeId]),
+
+    CONSTRAINT [FK_api_ApiUserScopes_ApiUser] 
+        FOREIGN KEY ([ApiUserId]) 
+        REFERENCES [dbo].[api_ApiUser]([ApiUserId]),
+
+    CONSTRAINT [FK_api_ApiUserScopes_ApiScopes] 
+        FOREIGN KEY ([ApiScopeId]) 
+        REFERENCES [dbo].[api_ApiScopes]([ApiScopeId])
 );
